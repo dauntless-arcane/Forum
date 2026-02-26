2ded2594cad33b3f693911cd67ec55c6d8d48842
Pratyush <79155396+Mpratyush54@users.noreply.github.com>
2026-02-26 23:22:11 +0530

    feat: add initial server setup in server.js

--- src/server.js
+++ src/server.js
@@ -187,7 +187,7 @@ app.use(cors({
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-bypass-token'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-bypass-token', 'x-api-key'],
 }));
 
 
