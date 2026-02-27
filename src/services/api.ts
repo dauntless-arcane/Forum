1a00dab8df566f9fc7674502e4d16a09f39075e7
Pratyush Mishra <79155396+Mpratyush54@users.noreply.github.com>
2026-02-27 10:58:01 +0530

    Switch API_URL to use environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { User, Question, Answer, AuthResponse } from '../types';
 
-// const API_URL = import.meta.env.VITE_API_URL || 'https://backend.capskengeri.com/api';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend.capskengeri.com/api';
+//const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
